8491c2204e48d3f034501b56ae27f87433d27169
yoonday <yoon2dev@gmail.com>
2024-11-13 16:32:42 +0900

    feat: 자동 클릭 기능 구현(코루틴)
    itemData에 등록해 놓은 이름과 비교하여 클릭 실행

--- Assets/Scripts/AutoClickManager.cs
+++ Assets/Scripts/AutoClickManager.cs
@@ -10,7 +10,7 @@ public class AutoClickManager : ClickManager // ClickManager 상속 받기
     private Coroutine coroutine;
     
     // 클릭 간격 설정 → 업데이트해서 클릭 간격 더 짧게 조정하기
-    public float clickInterval = 4.0f;
+    public float clickInterval = 0.2f;
 
     public Button beanAutoBtn;
     private Color beanInitialColor;
@@ -36,42 +36,42 @@ public class AutoClickManager : ClickManager // ClickManager 상속 받기
     }
 
 
-    //public void OnAutoClick(ItemData itemData) // UIManager - UpdateUnlock처럼 이름으로 찾기
-    //{
-    //    GameObject targetButton = GameObject.Find(itemData.autoBtnName);
-    //    // TODO :: 색상 변경하기
-    //    // Button buttonComponent = targetButton.GetComponent<Button>();
+    public void OnAutoClick(ItemData itemData) // UIManager - UpdateUnlock처럼 이름으로 찾기
+    {
+        GameObject targetButton = GameObject.Find(itemData.autoBtnName);
+        // TODO :: 색상 변경하기
+        // Button buttonComponent = targetButton.GetComponent<Button>();
 
-    //    if (coroutine != null) // 이미 실행 중이면 멈추기
-    //    {
-    //        StopCoroutine(coroutine);
-    //        coroutine = null;
-    //        isAutoBtnActivate = false;
-    //        // TODO :: 색상 원래 색으로 돌리기
-    //    }
-    //    else
-    //    {
-    //        coroutine = StartCoroutine(AutoClickCoroutine(itemData));
-    //        isAutoBtnActivate = true;
-    //        // TODO :: 버튼 활성화되었을 때 색 다르게 바꾸기
-    //    }
+        if (coroutine != null) // 이미 실행 중이면 멈추기
+        {
+            StopCoroutine(coroutine);
+            coroutine = null;
+            isAutoBtnActivate = false;
+            // TODO :: 색상 원래 색으로 돌리기
+        }
+        else
+        {
+            coroutine = StartCoroutine(AutoClickCoroutine(itemData));
+            isAutoBtnActivate = true;
+            // TODO :: 버튼 활성화되었을 때 색 다르게 바꾸기
+        }
 
-    //}
+    }
 
-    //private IEnumerator AutoClickCoroutine(ItemData itemData)
-    //{
+    private IEnumerator AutoClickCoroutine(ItemData itemData)
+    {
 
-    //    while (true) // 무한 반복
-    //    {
-    //        if (itemData.autoBtnName == "btn_Auto_Bean")
-    //        {
-    //            OnBeanClick();
-    //        }
-    //        else if (itemData.autoBtnName == "btn_Auto_Coffee")
-    //        {
-    //            OnCoffeeClick();
-    //        }
-    //        yield return new WaitForSeconds(clickInterval);
-    //    }
-    //}
+        while (true) // 무한 반복
+        {
+            if (itemData.autoBtnName == "btn_Auto_Bean")
+            {
+                OnBeanClick();
+            }
+            else if (itemData.autoBtnName == "btn_Auto_Coffee")
+            {
+                OnCoffeeClick();
+            }
+            yield return new WaitForSeconds(clickInterval);
+        }
+    }
 }
