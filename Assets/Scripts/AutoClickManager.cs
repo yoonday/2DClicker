079f467f20ba8826f6ec1b25e6bf99842dae76e7
yoonday <yoon2dev@gmail.com>
2024-11-13 16:21:53 +0900

    chore: 팝업 아이템 버튼 색상 변경
    - 버튼 비활성화 상태 : 배경 짙은 색으로 보이게 변경

--- Assets/Scripts/AutoClickManager.cs
+++ Assets/Scripts/AutoClickManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AutoClickManager : ClickManager // ClickManager 상속 받기
 {
@@ -11,33 +12,66 @@ public class AutoClickManager : ClickManager // ClickManager 상속 받기
     // 클릭 간격 설정 → 업데이트해서 클릭 간격 더 짧게 조정하기
     public float clickInterval = 4.0f;
 
+    public Button beanAutoBtn;
+    private Color beanInitialColor;
+
+    public Button coffeeAutoBtn;
+    private Color CoffeeInitialColor;
+  
+    private bool isAutoBtnActivate;
+
 
 
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void OnAutoClick()
-    {
-        if (coroutine != null)
+        // 색상 초기 세팅 값 저장
+        if (beanAutoBtn != null)
         {
-            StopCoroutine(coroutine);
+            beanInitialColor = beanAutoBtn.image.color;
         }
-
-        coroutine = StartCoroutine(AutoClickCoroutine());
-
+        if (coffeeAutoBtn != null)
+        {
+            CoffeeInitialColor = coffeeAutoBtn.image.color;
+        }
     }
 
-    private IEnumerator AutoClickCoroutine()
-    {
-        OnBeanClick();
-        yield return new WaitForSeconds(clickInterval);
-    }
+
+    //public void OnAutoClick(ItemData itemData) // UIManager - UpdateUnlock처럼 이름으로 찾기
+    //{
+    //    GameObject targetButton = GameObject.Find(itemData.autoBtnName);
+    //    // TODO :: 색상 변경하기
+    //    // Button buttonComponent = targetButton.GetComponent<Button>();
+
+    //    if (coroutine != null) // 이미 실행 중이면 멈추기
+    //    {
+    //        StopCoroutine(coroutine);
+    //        coroutine = null;
+    //        isAutoBtnActivate = false;
+    //        // TODO :: 색상 원래 색으로 돌리기
+    //    }
+    //    else
+    //    {
+    //        coroutine = StartCoroutine(AutoClickCoroutine(itemData));
+    //        isAutoBtnActivate = true;
+    //        // TODO :: 버튼 활성화되었을 때 색 다르게 바꾸기
+    //    }
+
+    //}
+
+    //private IEnumerator AutoClickCoroutine(ItemData itemData)
+    //{
+
+    //    while (true) // 무한 반복
+    //    {
+    //        if (itemData.autoBtnName == "btn_Auto_Bean")
+    //        {
+    //            OnBeanClick();
+    //        }
+    //        else if (itemData.autoBtnName == "btn_Auto_Coffee")
+    //        {
+    //            OnCoffeeClick();
+    //        }
+    //        yield return new WaitForSeconds(clickInterval);
+    //    }
+    //}
 }
